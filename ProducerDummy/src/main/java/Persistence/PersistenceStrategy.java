7676644c8daa9cb1c550419d0e6dc0f4382e3585
Francesco <francesco.mazzini432@gmail.com>
2022-05-09 20:49:56 +0200

    Written documentation in PersistenceStrategy class
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- ProducerDummy/src/main/java/Persistence/PersistenceStrategy.java
+++ ProducerDummy/src/main/java/Persistence/PersistenceStrategy.java
@@ -2,20 +2,25 @@ package Persistence;
 
 import Messages.AbstractMessage;
 
+/** Interface for the persistence strategy mechanisms */
 public interface PersistenceStrategy {
 
     /***
-     * Store the Messages.Message into your Persistence Mechasnium e.g File or Database
-     * @param sequenceNumber
-     * @param message
+     * Store the Messages.Message into your Persistence Mechanism e.g File or Database.
+     *
+     * @param sequenceNumber the sequence number of the message
+     * @param message message to be stored as a string
      */
     public void StoreMessage(int sequenceNumber, String message);
 
     /**
-     * e.g Create the File or the Database
+     * Create a the persistence mechanism, e.g create the File or the Database.
      */
     public void CreatePersistenceMechanism();
 
+    /**
+     * @return last AbstractMessage read from the persistence mechanism
+     */
     public AbstractMessage ReadLastMessage();
 
 }
