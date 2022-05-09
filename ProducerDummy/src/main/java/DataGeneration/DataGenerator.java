14d1f06c655302d6e6fbcb889d104ddb78270fc7
Francesco <francesco.mazzini432@gmail.com>
2022-05-09 18:45:54 +0200

    Written documentation in DataGenerator class
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- ProducerDummy/src/main/java/DataGeneration/DataGenerator.java
+++ ProducerDummy/src/main/java/DataGeneration/DataGenerator.java
@@ -2,11 +2,26 @@ package DataGeneration;
 
 import java.io.IOException;
 
-
+/***
+ * Data Generator interface
+ */
 public interface DataGenerator {
 
+    /**
+     * Read the next line from the file and return it as a String
+     *
+     * @return the line read
+     * */
     public String getData();
 
+    /**
+     * Read the line after a number of lines skipped and return it as a String.
+     * Used in recovery mode, to recover the last line read and read a new one.
+     *
+     * @param sequence_number number of lines to be skipped
+     *
+     * @return the line read
+     * */
     public String getData(int sequence_number);
 
 }
