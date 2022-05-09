a94604ccdb2aa816979c4d967cc4436ecb46a802
Francesco <francesco.mazzini432@gmail.com>
2022-05-09 20:51:40 +0200

    Updated documentation in Message class
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- ProducerDummy/src/main/java/Messages/Message.java
+++ ProducerDummy/src/main/java/Messages/Message.java
@@ -2,12 +2,14 @@ package Messages;
 
 import java.nio.charset.StandardCharsets;
 
+/** Message of generic type, without any structure */
 public class Message extends AbstractMessage {
 
     /**
-     * Constructor for Message
+     * Constructor for Message.
      *
-     * @param sequence_number sequence number of the messages
+     * @param sequence_number sequence number of the messages. Used to keep the cardinality of the messages in the order
+     *                        they are sent from the client
      * @param message message as a string
      */
     public Message(int sequence_number, String message) {
