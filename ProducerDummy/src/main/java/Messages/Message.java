b3788a36ddb34d532e4a851d8361cbc09a48c4be
Francesco <francesco.mazzini432@gmail.com>
2022-05-09 18:47:15 +0200

    Written documentation in Message class
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- ProducerDummy/src/main/java/Messages/Message.java
+++ ProducerDummy/src/main/java/Messages/Message.java
@@ -4,7 +4,12 @@ import java.nio.charset.StandardCharsets;
 
 public class Message extends AbstractMessage {
 
-
+    /**
+     * Constructor for Message
+     *
+     * @param sequence_number sequence number of the messages
+     * @param message message as a string
+     */
     public Message(int sequence_number, String message) {
         super(sequence_number, message);
     }
