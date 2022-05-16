27a69ab991e0609cb32bf760c10a203e2ec110ff
Francesco <francesco.mazzini432@gmail.com>
2022-05-16 17:57:35 +0200

    Written Javadoc on Hasher class
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- Blockchain/BlockchainCodeDraft/src/main/java/Hasher.java
+++ Blockchain/BlockchainCodeDraft/src/main/java/Hasher.java
@@ -1,14 +1,18 @@
-import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
-import java.util.logging.Level;
 import java.util.stream.Collectors;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class Hasher {
 
+    /**
+     * Hash a series of strings, concatenating them and then using SHA-256 (hash method used in cryptocurrencies)
+     *
+     * @param input The strings to be concatenated and then hashed
+     * @return The hash of the concatenated string
+     */
     public static String hashSHA256 (String ... input) {
 
         String plaintext = Arrays.stream(input)
