4629c117410c1ce400565b192f7cbc19b8d63fdc
Francesco <francesco.mazzini432@gmail.com>
2022-05-16 17:53:57 +0200

    Written Javadoc on Block class
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- Blockchain/BlockchainCodeDraft/src/main/java/Block.java
+++ Blockchain/BlockchainCodeDraft/src/main/java/Block.java
@@ -1,21 +1,35 @@
+import java.util.Date;
+
 public class Block {
-    private final String previousHashBlock;
-    private String hashBlock;
-    private final long timestamp;
-    private final String content;
-    private int nonce;
+    private final String previousHashBlock; /** contains the hash of the previous block */
+    private String hashBlock; /** contains the hash of the current block */
+    private final long timestamp; /** contains the date and time of when the block was created */
+    private final String content; /** content of the current block */
+    private int nonce; /** arbitrary number to be used in cryptography */
 
     public Block (String previousHashBlock, String content) {
         this.previousHashBlock = previousHashBlock;
         this.content = content;
-        this.timestamp = System.currentTimeMillis();
+        this.timestamp = new Date().getTime();
         this.hashBlock = calcHash();
     }
 
+    /**
+     * Calculates the hash of the current block calling {@link Hasher#hashSHA256} method
+     *
+     * @return the hash of the current block
+     * */
     private String calcHash() {
         return Hasher.hashSHA256(previousHashBlock, Long.toString(timestamp), content);
     }
 
+    /**
+     * Mine the current block using a prefix with which the hash has to begin.
+     * The nonce is incremented until the hash of the block begins with the prefix of 0's.
+     *
+     * @param prefix
+     * @return the hash of the current block
+     */
     public String mineBlock(int prefix) {
         String prefixString = new String(new char[prefix]).replace('\0', '0');
         while (!hashBlock.substring(0, prefix).equals(prefixString)) {
@@ -25,6 +39,9 @@ public class Block {
         return hashBlock;
     }
 
+    /**
+     * @return hash of the current block saved in the block
+     */
     public String getHashBlock () {
         return hashBlock;
     }
