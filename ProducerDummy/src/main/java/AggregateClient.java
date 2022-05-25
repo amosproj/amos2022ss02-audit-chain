dc32bb5efa2b240b8c1a85beb8190b0487871066
Francesco Mazzini <79516317+francescomazzini@users.noreply.github.com>
2022-05-25 11:15:52 +0200

    Restoring situation before the mistake of merging dev into branch

--- ProducerDummy/src/main/java/AggregateClient.java
+++ ProducerDummy/src/main/java/AggregateClient.java
@@ -63,7 +63,7 @@ public class AggregateClient extends AbstractClient {
                     }
                 }
             long end = System.nanoTime();
-            System.out.format("Messages until the position " + (this.sequence_number + 1)  + " are sent correctly and "
+            System.out.format("Messages until the position " + (this.sequence_number)  + " are sent correctly and "
                     + "received by the Queue in " + Duration.ofNanos(end - start).toMillis());
            }
       return;
