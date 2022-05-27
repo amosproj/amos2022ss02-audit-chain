ffef6c17cdd01f820436e5eb7e59713394a59dc4
Francesco <francesco.mazzini432@gmail.com>
2022-05-27 18:46:51 +0200

    Edited the structure of the block in the blockchain such that now they contain an hashmap os messages inside
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- Blockchain/BlockchainImplementation/src/main/java/Blockchain/Blocks/Block.java
+++ Blockchain/BlockchainImplementation/src/main/java/Blockchain/Blocks/Block.java
@@ -2,24 +2,45 @@ package Blockchain.Blocks;
 
 import Blockchain.Hashing.Hasher;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Block implements BlockInterface {
 
     private final String previousHashBlock; /** contains the hash of the previous block */
     private String hashBlock; /** contains the hash of the current block */
     private final long timestamp; /** contains the date and time of when the block was created */
-    private final String sequence_number; /** contains the sequence number of the message */
-    private final String message; /** content of the current block */
+    private Map<String, SubBlock> data;
     private int nonce; /** arbitrary number to be used in cryptography */
 
-    public Block(String previousHashBlock, String sequence_number, String message, int prefix) {
+    public Block(String previousHashBlock, int prefix, File data) {
         this.previousHashBlock = previousHashBlock;
-        this.sequence_number = sequence_number;
-        this.message = message;
+        extractDataFromFile(data);
         this.timestamp = new Date().getTime();
         this.hashBlock = calcHash();
         mineBlock(prefix);
+    }
+
+//    public Block(String previousHashBlock, int prefix, String JSON) {
+//        this.previousHashBlock = previousHashBlock;
+//        this.data = extractDataFromFile(data);
+//        this.timestamp = new Date().getTime();
+//        this.hashBlock = calcHash();
+//        mineBlock(prefix);
+//    }
+
+    private String getDataHash () {
+        String result = "";
+
+        for (String key : data.keySet()) {
+            result += key + data.get(key);
+        }
+        return Hasher.hashSHA256(result);
     }
 
     /**
@@ -29,7 +50,7 @@ public class Block implements BlockInterface {
      * */
     private String calcHash() {
         return Hasher.hashSHA256(previousHashBlock, Long.toString(timestamp),
-                Integer.toString(nonce), sequence_number, message);
+                Integer.toString(nonce), getDataHash());
     }
 
     /**
@@ -48,6 +69,12 @@ public class Block implements BlockInterface {
         return hashBlock;
     }
 
+    private void extractDataFromFile(File file)  {
+        data = new HashMap<>();
+
+       // BufferedReader reader = new BufferedReader(new FileReader(file));
+    }
+
     /**
      * @return hash of the current block saved in the block
      */
@@ -59,9 +86,7 @@ public class Block implements BlockInterface {
     @Override
     public String getPreviousHashBlock () { return previousHashBlock; }
 
-    public String getSequenceNumber () { return sequence_number; }
-
-    public String getMessage () { return message; }
+    public Map<String, SubBlock> getData () { return data; }
 
     public long getTimestamp () { return timestamp; }
 
