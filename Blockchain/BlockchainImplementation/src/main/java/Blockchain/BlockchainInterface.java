d4291b73b6451c2e97bf2f3b018d4597e315de6f
Francesco <francesco.mazzini432@gmail.com>
2022-05-27 18:45:18 +0200

    Edited the way of interaction with the blockchain
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- Blockchain/BlockchainImplementation/src/main/java/Blockchain/BlockchainInterface.java
+++ Blockchain/BlockchainImplementation/src/main/java/Blockchain/BlockchainInterface.java
@@ -2,9 +2,11 @@ package Blockchain;
 
 import Blockchain.Blocks.Block;
 
+import java.io.File;
+
 public interface BlockchainInterface {
 
-    public void addABlock(String sequence_number, String message);
+    public void addABlock(File transactions);
 
     public String getLastBlockHash();
 
