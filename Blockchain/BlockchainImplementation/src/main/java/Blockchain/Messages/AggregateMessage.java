561787f7e973381d55c5f4be81b5b71be6760704
Francesco <francesco.mazzini432@gmail.com>
2022-05-28 20:21:33 +0200

    Copied files from other part of the projects, so to call them in other classes. TO BE SOLVED: this is not ok we have to find a solution to do that

---
+++ Blockchain/BlockchainImplementation/src/main/java/Blockchain/Messages/AggregateMessage.java
@@ -0,0 +1,68 @@
+package Messages;
+
+import java.util.Vector;
+
+public class AggregateMessage implements Message {
+
+
+    Vector<Message> messages = new Vector<>();
+
+
+    public AggregateMessage() {
+
+    }
+
+    public AggregateMessage(int sequence_number, String message) {
+        this.addMessage(new JsonMessage(sequence_number, message));
+    }
+
+    public AggregateMessage(int sequence_number) {
+        this.addMessage(new JsonMessage(sequence_number, ""));
+    }
+
+    public void addMessage(Message message) {
+        this.messages.add(message);
+    }
+
+
+    @Override
+    public Message getMessageObject() {
+        return this;
+    }
+
+    @Override
+    public void formatMessage(int sequence_number, String message) {
+        return;
+    }
+
+    @Override
+    public int getSequence_number() {
+        return this.messages.get(messages.size() - 1).getSequence_number();
+    }
+
+    @Override
+    public void setSequence_number(int sequence_number) {
+        this.messages.get(this.messages.size() - 1).setSequence_number(sequence_number);
+        return;
+    }
+
+    @Override
+    public String getMessage() {
+        return messages.get(messages.size() - 1).getMessage();
+    }
+
+    @Override
+    public void setMessage(String message) {
+        this.messages.get(this.messages.size() - 1).setMessage("test");
+        return;
+    }
+
+    public int getMessageSize() {
+        return messages.size();
+    }
+
+    public Vector<Message> getMessages(){
+        return this.messages;
+    }
+
+}
---
+++ Blockchain/BlockchainImplementation/src/main/java/Blockchain/Persistence/NullObjectPersistenceStrategy.java
@@ -0,0 +1,31 @@
+package Persistence;
+
+
+import Messages.Message;
+import Messages.SimpleMessage;
+
+/***
+ * This Implementation should be used for Debug purpose because you don´t want to write every message into your Hdd.
+ */
+
+public class NullObjectPersistenceStrategy implements PersistenceStrategy {
+
+
+    public NullObjectPersistenceStrategy(String filepath,String fileName){
+        return;
+    }
+    @Override
+    public void StoreMessage(Message message) {
+        return;
+    }
+
+    @Override
+    public void CreatePersistenceMechanism() {
+        return;
+    }
+
+    @Override
+    public Message ReadLastMessage() {
+        return new SimpleMessage(0, "DEBUG");
+    }
+}
