d2810f4de06d2e5ea44d773aaabf95434d5e64c2
Francesco <francesco.mazzini432@gmail.com>
2022-05-28 20:35:07 +0200

    Subblocks are now just hashed and linked with a previousHashBlock so to rebuild the hashed list; They are not a proper blockchain anymore; TO BE DONE: understand what it has to be done with Hmac for messages in a subblock
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- Blockchain/BlockchainImplementation/src/main/java/Blockchain/Blocks/SubBlock.java
+++ Blockchain/BlockchainImplementation/src/main/java/Blockchain/Blocks/SubBlock.java
@@ -9,10 +9,10 @@ public class SubBlock {
     private final String previousHashBlock; /** contains the hash of the previous block */
     private String hashBlock; /** contains the hash of the current block */
     private final long timestamp; /** contains the date and time of when the block was created */
-    private final String sequence_number; /** contains the sequence number of the message */
+    private final int sequence_number; /** contains the sequence number of the message */
     private final String message; /** content of the current block */
 
-    public SubBlock(String previousHashBlock, String sequence_number, String message) {
+    public SubBlock(String previousHashBlock, int sequence_number, String message) {
         this.previousHashBlock = previousHashBlock;
         this.sequence_number = sequence_number;
         this.message = message;
@@ -22,7 +22,7 @@ public class SubBlock {
 
 
     private String calcHash() {
-        return Hasher.hashSHA256(previousHashBlock, Long.toString(timestamp), sequence_number, message);
+        return Hasher.hashSHA256(previousHashBlock, Long.toString(timestamp), Integer.toString(sequence_number), message);
     }
 
     public String getHashBlock () {
@@ -31,7 +31,7 @@ public class SubBlock {
 
     public String getPreviousHashBlock () { return previousHashBlock; }
 
-    public String getSequenceNumber () { return sequence_number; }
+    public int getSequenceNumber () { return sequence_number; }
 
     public String getMessage () { return message; }
 
