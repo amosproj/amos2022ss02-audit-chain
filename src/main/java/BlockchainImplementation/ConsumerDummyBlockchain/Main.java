f290fa31c7a178d376e8218be007f5ce2dcac36f
Francesco <francesco.mazzini432@gmail.com>
2022-05-30 15:12:54 +0200

    Merging Blockchain Part
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

---
+++ src/main/java/BlockchainImplementation/ConsumerDummyBlockchain/Main.java
@@ -0,0 +1,15 @@
+package BlockchainImplementation.ConsumerDummyBlockchain;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConsumerClientBlockchain client = new ConsumerClientBlockchain();
+        client.start();
+        return;
+    }
+
+
+}
