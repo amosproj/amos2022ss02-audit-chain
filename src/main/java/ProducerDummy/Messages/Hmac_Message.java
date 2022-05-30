79029a2a59faff0c7de78e2991e0fd24bf6dd831
Lavicola <lavicola7777@t-online.de>
2022-05-30 12:37:28 +0200

    added code

---
+++ src/main/java/ProducerDummy/Messages/Hmac_Message.java
@@ -0,0 +1,15 @@
+package ProducerDummy.Messages;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public interface Hmac_Message extends Message {
+
+
+
+    public boolean verifyMAC(String algorithm, Message message, String key) throws NoSuchAlgorithmException, InvalidKeyException;
+    public String calculateMac(String algorithm,Message message, String key) throws NoSuchAlgorithmException, InvalidKeyException;
+
+
+
+}
