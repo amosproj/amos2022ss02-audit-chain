02084b940154a7ce9c50b015a5fd18c97257937f
Lavicola <lavicola7777@t-online.de>
2022-06-14 22:53:47 +0200

    Blockchain Consumer is now using the same config.properties like ProducerDummy

--- src/main/java/BlockchainImplementation/ConsumerDummyBlockchain/Main.java
+++ src/main/java/BlockchainImplementation/ConsumerDummyBlockchain/Main.java
@@ -1,13 +1,33 @@
 package BlockchainImplementation.ConsumerDummyBlockchain;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
 import java.util.concurrent.TimeoutException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, TimeoutException {
 
-        ConsumerClientBlockchain client = new ConsumerClientBlockchain();
+        String filepath = Paths.get("src", "main", "resources","ProducerDummy").toString();
+        String filename = "config.properties";
+
+
+        Path config_path = Paths.get(System.getProperty("user.dir"), filepath, filename);
+        Properties p = new Properties();
+        FileReader reader = new FileReader(config_path.toString());
+        p.load(reader);
+
+        String HOST = p.getProperty("HOST");
+        int PORT = Integer.parseInt(p.getProperty("PORT"));
+        String USER = p.getProperty("USERNAME");
+        String PASSWORD = p.getProperty("PASSWORD");
+        String queue_name = "FAKE";
+
+
+        ConsumerClientBlockchain client = new ConsumerClientBlockchain(HOST, PORT, USER, PASSWORD, queue_name);
         client.start();
         return;
 
